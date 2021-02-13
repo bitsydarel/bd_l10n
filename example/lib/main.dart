41b55c4f6fe7f8412d610278fc2225e7bae7cc20
bitsydarel <bitsydarel@gmail.com>
2021-02-13 05:45:13 +0200

    Updated example app with guideline on how to build it.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,8 +6,11 @@
  */
 
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/localization/application/application_feature.dart';
-import 'package:flutter_demo/localization/authentication/authentication_feature.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+// TODO: un comment after code generation.
+// import 'package:flutter_demo/localization/application/application_feature.dart'; Uncomment after generated code.
+// import 'package:flutter_demo/localization/authentication/authentication_feature.dart'; Uncomment after generated code.
 
 void main() {
   runApp(MyApp());
@@ -20,51 +23,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       routes: <String, WidgetBuilder>{
         '/': (BuildContext routeContext) {
-          final localization = ApplicationFeature.of(routeContext);
+          // TODO: uncomment after code generation.
+          // final ApplicationFeature localization = ApplicationFeature.of(
+          //   routeContext,
+          // );
 
-          return MyHomePage(title: localization.applicationTitle);
+          // return MyHomePage(title: localization.applicationTitle);
+          return MyHomePage(title: 'Home tool');
         },
         '/login': (BuildContext routeContext) {
           return LoginPage();
         },
       },
       localizationsDelegates: [
-        ApplicationFeature.delegate,
-        AuthenticationFeature.delegate,
+        // TODO: uncomment after code generation.
+        // ApplicationFeature.delegate,
+        // AuthenticationFeature.delegate,
+        ...GlobalMaterialLocalizations.delegates
       ],
-      supportedLocales: ApplicationFeature.supportedLocales,
+      // TODO uncomment after code generation.
+      // supportedLocales: ApplicationFeature.supportedLocales,
+      supportedLocales: <Locale>[Locale.fromSubtags(languageCode: 'en')],
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -77,11 +67,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
 
       if (_counter > 5) {
@@ -92,41 +77,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final localization = ApplicationFeature.of(context);
+    // TODO: uncomment after code generation.
+    // final localization = ApplicationFeature.of(context);
 
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(localization.homeDescription),
+            // TODO uncomment after code generation.
+            // Text(localization.homeDescription),
+            Text('Clicked: '),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
@@ -136,7 +100,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: localization.incrementButton,
+        // TODO: uncomment after code generation.
+        // tooltip: localization.incrementButton,
+        tooltip: 'Incremented',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
@@ -146,7 +112,8 @@ class _MyHomePageState extends State<MyHomePage> {
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final localization = AuthenticationFeature.of(context);
+    // TODO: uncomment after code generation.
+    // final localization = AuthenticationFeature.of(context);
 
     return SafeArea(
       child: Scaffold(
@@ -154,13 +121,19 @@ class LoginPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextFormField(initialValue: localization.userNameField),
+            // TODO: uncomment after code generation and replace.
+            // TextFormField(initialValue: localization.userNameField),
+            TextFormField(initialValue: 'Enter username'),
             const SizedBox(height: 16),
-            TextFormField(initialValue: localization.passwordField),
+            // TODO: uncomment after code generation and replace.
+            // TextFormField(initialValue: localization.passwordField),
+            TextFormField(initialValue: 'Enter password'),
             const SizedBox(height: 16),
             ElevatedButton(
               onPressed: null,
-              child: Text(localization.loginButton),
+              // TODO: uncomment after code generation and replace.
+              // child: Text(localization.loginButton),
+              child: Text('Login'),
             ),
           ],
         ),
